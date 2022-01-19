6d386ff874451a05b22222e863af959221376f18
HyungjunJeon <maru105kr@gmail.com>
2022-01-19 23:12:56 +0900

    Realize drag function

--- dist/components/page/page.js
+++ dist/components/page/page.js
@@ -20,12 +20,34 @@ export class PageItemComponent extends BaseComponent {
         this.element.addEventListener("dragend", (event) => {
             this.onDragEnd(event);
         });
+        this.element.addEventListener("dragenter", (event) => {
+            this.onDragEnter(event);
+        });
+        this.element.addEventListener("dragleave", (event) => {
+            this.onDragLeave(event);
+        });
     }
-    onDragStart(event) {
-        console.log("dragstart", event);
+    onDragStart(_) {
+        this.notifyDragObservers("start");
+        this.element.classList.add("lifted");
     }
-    onDragEnd(event) {
-        console.log("dragend", event);
+    onDragEnd(_) {
+        this.notifyDragObservers("stop");
+        this.element.classList.remove("lifted");
+    }
+    onDragEnter(_) {
+        this.notifyDragObservers("enter");
+        this.element.classList.add("drop-area");
+    }
+    onDragLeave(_) {
+        this.notifyDragObservers("leave");
+        this.element.classList.remove("drop-area");
+    }
+    onDropped() {
+        this.element.classList.remove("drop-area");
+    }
+    notifyDragObservers(state) {
+        this.dragStateListener && this.dragStateListener(this, state);
     }
     addChild(child) {
         const container = this.element.querySelector(".page-item__body");
@@ -34,25 +56,48 @@ export class PageItemComponent extends BaseComponent {
     setOnCloseListener(listener) {
         this.closeListener = listener;
     }
+    setOnDragStateListener(listener) {
+        this.dragStateListener = listener;
+    }
+    muteChildren(state) {
+        if (state === "mute") {
+            this.element.classList.add("mute-children");
+        }
+        else {
+            this.element.classList.remove("mute-children");
+        }
+    }
+    getBoundingRect() {
+        return this.element.getBoundingClientRect();
+    }
 }
 export class PageComponent extends BaseComponent {
     constructor(pageItemConstructor) {
         super(`<ul class="page"></ul>`);
         this.pageItemConstructor = pageItemConstructor;
+        this.children = new Set();
         this.element.addEventListener("dragover", (event) => {
             this.onDragOver(event);
         });
         this.element.addEventListener("drop", (event) => {
-            this.onDragDrop(event);
+            this.onDrop(event);
         });
     }
     onDragOver(event) {
         event.preventDefault();
-        console.log("dragover", event);
     }
-    onDragDrop(event) {
+    onDrop(event) {
         event.preventDefault();
-        console.log("drop", event);
+        if (!this.dropTarget) {
+            return;
+        }
+        if (this.dragTarget && this.dragTarget !== this.dropTarget) {
+            const dropY = event.clientY;
+            const srcElement = this.dragTarget.getBoundingRect();
+            this.dragTarget.removeFrom(this.element);
+            this.dropTarget.attach(this.dragTarget, dropY < srcElement.y ? "beforebegin" : "afterend");
+        }
+        this.dropTarget.onDropped();
     }
     addChild(section) {
         const item = new this.pageItemConstructor();
@@ -60,6 +105,33 @@ export class PageComponent extends BaseComponent {
         item.attachTo(this.element, "beforeend");
         item.setOnCloseListener(() => {
             item.removeFrom(this.element);
+            this.children.delete(item);
+        });
+        this.children.add(item);
+        item.setOnDragStateListener((target, state) => {
+            switch (state) {
+                case "start":
+                    this.dragTarget = target;
+                    this.updateSections("mute");
+                    break;
+                case "stop":
+                    this.dragTarget = undefined;
+                    this.updateSections("unmute");
+                    break;
+                case "enter":
+                    this.dropTarget = target;
+                    break;
+                case "leave":
+                    this.dropTarget = undefined;
+                    break;
+                default:
+                    throw new Error(`unsupported state: ${state}`);
+            }
+        });
+    }
+    updateSections(state) {
+        this.children.forEach((section) => {
+            section.muteChildren(state);
         });
     }
 }
